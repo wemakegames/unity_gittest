7f2ee40cad5a20fcb69e855aedb01a12c70ed25e
andreadst <andreadst@hotmail.com>
2014-07-24 18:37:09 +0200

    added menu and camera follow on leading player

--- Assets/scripts/camera_mvt.cs
+++ Assets/scripts/camera_mvt.cs
@@ -3,21 +3,33 @@ using System.Collections;
 
 public class camera_mvt : MonoBehaviour {
 
-	public Transform myTarget;
+	private Transform myTarget;
+	private GameObject p1;
+	private GameObject p2;
 
+	public float camSpeed = 2.0f;
 
 	// Use this for initialization
 	void Start () {
-	
+		p1 = GameObject.Find("player1");
+		p2 = GameObject.Find("player2");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		Vector3 target_position = myTarget.localPosition;
-		target_position.x = myTarget.transform.position.x;
-		target_position.y = 0;
-		target_position.z = myTarget.transform.position.z - 10;
 
-		transform.localPosition = target_position;
+	void FixedUpdate () {
+
+		if (p1.transform.localPosition.x > p2.transform.localPosition.x) {
+			myTarget =	p1.transform;
+		}else
+		{
+			myTarget = p2.transform;
+		}
+
+		//temp variable to store local position
+		Vector3 temp = transform.localPosition;
+		temp.x = Mathf.Lerp(transform.localPosition.x, myTarget.transform.localPosition.x, Time.deltaTime * camSpeed);
+		transform.localPosition = temp;
+
 	}
 }
--- Assets/scripts/player_mvt.cs
+++ Assets/scripts/player_mvt.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class bear_movement : MonoBehaviour {
+public class player_mvt : MonoBehaviour {
 
 	
 	private float speed_h;
@@ -14,9 +14,13 @@ public class bear_movement : MonoBehaviour {
 	public float gravity = 5.0f;
 	public float gravity_max = -1.0f;
 
-	private float button1;
-	private float button2;
-	
+	private bool button1;
+	private bool button2;
+	private int button_next = 1;
+
+	public string button1_name;
+	public string button2_name;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -25,52 +29,72 @@ public class bear_movement : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		GetControls();
-		ApplyForces();
-
+		CalcForces();
+		rigidbody2D.AddForce(new Vector2(speed_h,speed_v));
 	}
 
 
 
 	void GetControls(){
-		/*speed_h = Input.GetAxis("Horizontal") * 5;
-		speed_v = Input.GetAxis("Vertical") * 5;*/
-		button1 = Input.GetAxis("Fire1");
-		button2 = Input.GetAxis("Fire2");
-
+		button1 = Input.GetButton(button1_name);
+		button2 = Input.GetButton(button2_name);
 	}
 
-	void ApplyForces(){
+	void CalcForces(){
 		//check button
-		if (button1 > 0.5) { 
+		if (button1 && button2) { 
+			if  (speed_h < maxSpeed_h){
+				//speed_h += accel_h/4;
+			}
+			if (speed_v < maxSpeed_v) {
+				speed_v += accel_v;	
+			}
+		}
+		else if (button1 && button_next == 1) { 
 			if  (speed_h < maxSpeed_h){
 				speed_h += accel_h;
 			}
-			
+			if (speed_v < maxSpeed_v) {
+				speed_v += accel_v;	
+			}
+			button_next = 2;
+
+		}
+		else if (button2 && button_next == 2) { 
+			if  (speed_h < maxSpeed_h){
+				speed_h += accel_h;
+			}
 			if (speed_v < maxSpeed_v) {
 				speed_v += accel_v;	
 			}		
+			button_next = 1;
 		}
 
-		//DECREASE
-		if ((speed_h > 0) && (speed_h < slowdown_h)){
-			speed_h = 0;
-
-		}else if ((speed_h > 0) && (speed_h > slowdown_h)){
-			speed_h -= slowdown_h;
-		}
-
-
-			
 		if (speed_v > gravity_max){
 			speed_v -= gravity;
 		}
 
 
 
-		rigidbody2D.AddForce(new Vector2(speed_h,speed_v));
+
+
+
+
+		//DECREASE
+		if ((speed_h > 0) && (speed_h < slowdown_h)){
+			speed_h = 0;
+
+		}else if ((speed_h > 0) && (speed_h >= slowdown_h)){
+			speed_h -= slowdown_h;
+		}
+
+
+			
+
+
+
+
 	}
-
-
 
 
 	/*void Flip(){
