95f40a930f6545733b96d54ee60908128b837c5b
andreadst <andreadst@hotmail.com>
2014-07-25 17:53:36 +0200

    fixed bug with menu script and renamed it

--- Assets/scripts/menu_manager.cs
+++ Assets/scripts/menu_manager.cs
@@ -1,28 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
-
-
-public class menu : MonoBehaviour {
-
+public class menu_manager : MonoBehaviour {
+	
 	private bool button1;
 	private bool button2;
-
+	
 	// Use this for initialization
 	void Start () {
-
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		CheckControls();
 	}
-
+	
 	void CheckControls(){
 		button1 = Input.GetButton("Fire1");
 		button2 = Input.GetButton("Fire2");
-
+		
 		if (button1 || button2 ) {
 			Application.LoadLevel("game");
 		}
