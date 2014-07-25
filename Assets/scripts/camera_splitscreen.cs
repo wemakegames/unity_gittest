48106b6e8266c77de029eb48040c35d4324668bd
andreadst <andreadst@hotmail.com>
2014-07-25 12:56:55 +0200

    splitscreen added
    *note: the headplayer follow camera script is still present

---
+++ Assets/scripts/camera_splitscreen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera_splitscreen : MonoBehaviour {
+
+	public GameObject myTarget;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	
+	void FixedUpdate () {
+		
+		Vector3 temp_coord = transform.localPosition;
+		temp_coord.x = myTarget.transform.localPosition.x;
+		transform.localPosition = temp_coord;
+		
+	}
+}
