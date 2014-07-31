5ccb4cade96be6d6d5bbcb97851eac417c616515
wemakegames <andreadst@hotmail.com>
2014-07-31 13:00:46 +0200

    added full level generation

--- Assets/scripts/level_generator.cs
+++ Assets/scripts/level_generator.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class level_generator : MonoBehaviour {
 	
 	public GameObject tiles_0;
+	public Sprite[] tiles;
 	public int level_step = 0;
 	int[][]map_data;
 	
@@ -11,9 +12,17 @@ public class level_generator : MonoBehaviour {
 	void Start () {
 		levels the_levels;
 		the_levels = GetComponent<levels> ();
+
+		tiles = Resources.LoadAll<Sprite> ("sprite");
+		print (tiles.Length);
+
+		//tiles_0.GetComponent<SpriteRenderer> ().sprite = tiles [0];
+	
+		//SpriteRenderer tile_sprite_renderer = tiles_0.GetComponent<SpriteRenderer> ();
+		//tile_sprite_renderer.sprite = tiles[4];
+		//tile_sprite_renderer.color = new Color (0f, 0f, 0f, 1f);
 		the_levels.Start ();
 		map_data = the_levels.map_data_array;
-		print (map_data [0] [0]);
 		GenerateLevel (0);
 		
 	}
@@ -23,28 +32,30 @@ public class level_generator : MonoBehaviour {
 		if (Input.GetKeyDown ("space")) {
 			level_step++;
 			//GenerateLevel(level_step);
-			print("DING");
 			print(level_step);
 		}
 		
 	}
 	
 	void GenerateLevel(int the_step){
-		int x, y;
-		
-		for (y = 0; y < 12; y++){
-			for (x = 0; x < 96; x++)
-			{
-				if(map_data[0][y * 96 + x] == 5){
-					
-					Instantiate(tiles_0, new Vector3((1f * x)-96f*the_step,( 1f * -y) +11f, 0), Quaternion.identity);
-					
-				}else if(map_data[0][y * 96 + x] == 6){
-					
+		int x, y, z;
+
+
+
+		for (z = 0; z < 15; z++) {
+			the_step = z;
+				for (y = 0; y < 12; y++) {
+						for (x = 0; x < 96; x++) {
+								if (map_data [0] [y * 96 + x] == 5) {
+										Instantiate (tiles_0, new Vector3 ((1f * x) + 96f * the_step, (1f * -y) + 11f, 0), Quaternion.identity);
+										
+
+								} else if (map_data [0] [y * 96 + x] == 6) {
+										
+								}
+						}
 				}
-			}
 		}
-		print ("DONE");
 	}
 }
 
