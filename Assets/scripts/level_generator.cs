606fd5b8ad83648401a8d14dd4891dee935f73af
andreadst <andreadst@hotmail.com>
2014-08-05 11:45:29 +0200

    replaced huge IF with switch case

--- Assets/scripts/level_generator.cs
+++ Assets/scripts/level_generator.cs
@@ -34,31 +34,50 @@ public class level_generator : MonoBehaviour {
 
 		for (z = 0; z < chunks; z++) 
 		
-		{	
-		
+		{			
 			the_step = z;
 
 			for (y = 0; y < 12; y++) {
 				for (x = 0; x < 96; x++) {
 					tiles_0.GetComponent<BoxCollider2D> ().enabled = true;
-					if (map_data [z] [y * 96 + x] == 5) {
-							tiles_0.GetComponent<SpriteRenderer> ().sprite = tiles [4];					
-					} else if (map_data [z] [y * 96 + x] == 6) {
-							tiles_0.GetComponent<SpriteRenderer> ().sprite = tiles [5];
-							tiles_0.GetComponent<BoxCollider2D> ().enabled = false;
-					} else if (map_data [z] [y * 96 + x] == 4) {
+
+					switch (map_data [z] [y * 96 + x]) {
+						case 1:			//dark blue block
+							tiles_0.GetComponent<SpriteRenderer> ().sprite = tiles [0];
+							break;
+						case 2:			//white block
+							
+							tiles_0.GetComponent<SpriteRenderer> ().sprite = tiles [1];
+							break;
+						case 3:			//red block
+							tiles_0.GetComponent<SpriteRenderer> ().sprite = tiles [2];
+							break;
+						case 4:			//yellow block
 							tiles_0.GetComponent<SpriteRenderer> ().sprite = tiles [3];
-					} else if (map_data [z] [y * 96 + x] == 2) {
-						tiles_0.GetComponent<SpriteRenderer> ().sprite = tiles [1];
-					} else if (map_data [z] [y * 96 + x] == 3) {
-						tiles_0.GetComponent<SpriteRenderer> ().sprite = tiles [2];
-					}else if (map_data [z] [y * 96 + x] == 7) {
-						tiles_0.GetComponent<SpriteRenderer> ().sprite = tiles [6];
-					}else if (map_data [z] [y * 96 + x] == 8) {
-						tiles_0.GetComponent<SpriteRenderer> ().sprite = tiles [7];
+							break;
+						case 5:			//aqua blue block
+							tiles_0.GetComponent<SpriteRenderer> ().sprite = tiles [4];
+							break;
+						case 6:			//hearts
+							tiles_0.GetComponent<SpriteRenderer> ().sprite = tiles [5];							
+							tiles_0.GetComponent<BoxCollider2D> ().isTrigger = true;
+							tiles_0.GetComponent<BoxCollider2D> ().tag = "heart";
+							break;
+						case 7:			//green block
+							tiles_0.GetComponent<SpriteRenderer> ().sprite = tiles [6];
+							break;
+						case 8:			//pink
+							tiles_0.GetComponent<SpriteRenderer> ().sprite = tiles [7];
+							break;
 					}
 
-					if (map_data [z] [y * 96 + x] != 0){
+					if (map_data [z] [y * 96 + x] != 0){    //create the block
+
+						if (tiles_0.GetComponent<SpriteRenderer> ().sprite != tiles [5]){
+							tiles_0.GetComponent<BoxCollider2D> ().isTrigger = false;
+							tiles_0.GetComponent<BoxCollider2D> ().tag = "Untagged";
+						}
+
 						Instantiate (tiles_0, new Vector3 ((1f * x) + 96f * the_step, (1f * -y) + 11f, 0), Quaternion.identity);									
 					}
 				}
