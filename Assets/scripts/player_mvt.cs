1c317d837c24e241e873596fd5452e40a587e2ed
andreadst <andreadst@hotmail.com>
2014-08-06 21:12:04 +0200

    tweaked gameplay to be closer to Love version

--- Assets/scripts/player_mvt.cs
+++ Assets/scripts/player_mvt.cs
@@ -31,7 +31,8 @@ public class player_mvt : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		GetControls();
-		CalcForces();
+		CalcVForce();
+		CalcHForce();
 		rigidbody2D.AddForce(new Vector2(speed_h,speed_v));
 	}
 
@@ -42,26 +43,12 @@ public class player_mvt : MonoBehaviour {
 		button2 = Input.GetButton(button2_name);
 	}
 
-	void CalcForces(){
+	void CalcVForce(){
 		//check button
 		if (button1 && button2) { 
 			if (speed_v < maxSpeed_v) {
 				speed_v += accel_v;
 			}
-		}
-		else if (button1 && button_next == 1) { 
-			if  (speed_h < maxSpeed_h){
-				speed_h += accel_h;
-				GetComponent<SpriteRenderer>().sprite = spr1;
-			}
-			button_next = 2;
-		}
-		else if (button2 && button_next == 2) { 
-			if  (speed_h < maxSpeed_h){
-				speed_h += accel_h;
-				GetComponent<SpriteRenderer>().sprite = spr2;
-			}		
-			button_next = 1;
 		}
 
 		if (speed_v > max_slowdown_v){
@@ -69,7 +56,27 @@ public class player_mvt : MonoBehaviour {
 		} else {
 			speed_v = max_slowdown_v;
 		}
+	}
 
+
+	void CalcHForce(){
+		//check button
+
+		if ( button1 && !button2 && button_next == 1 ) { 
+			if  ( speed_h < maxSpeed_h ){
+				speed_h += accel_h;
+				GetComponent<SpriteRenderer>().sprite = spr1;
+			}
+			button_next = 2;
+		}
+		else if ( !button1 && button2 && button_next == 2 ) { 
+			if  ( speed_h < maxSpeed_h ){
+				speed_h += accel_h;
+				GetComponent<SpriteRenderer>().sprite = spr2;
+			}		
+			button_next = 1;
+		}
+		
 		if (speed_h > 0){
 			speed_h -= slowdown_h ;
 		} else {
