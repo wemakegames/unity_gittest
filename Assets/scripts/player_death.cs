23e8bf3432d8f7e59c4f71651ae1eefd9abcde65
andreadst <andreadst@hotmail.com>
2014-08-06 12:16:31 +0200

    fixed a bug with particles triggers centralized in game manager

--- Assets/scripts/player_death.cs
+++ Assets/scripts/player_death.cs
@@ -6,8 +6,6 @@ using System.Collections;
 public class player_death : MonoBehaviour {
 
 	public GameObject mark;
-	//public GameObject part_gibs;
-	public GameObject part_blood;
 
 	// Use this for initialization
 	void Start () {
@@ -18,33 +16,21 @@ public class player_death : MonoBehaviour {
 	void Update () {
 		if (mark && (transform.position.x > mark.transform.position.x)) {
 			kill_player();
-
-
 		}
 	}
 
 	void kill_player () {
 		this.renderer.enabled = false;
-		ParticleSystem[] part_gibs = GetComponentsInChildren<ParticleSystem>();
 
-		for (int i=0; i < part_gibs.Length; i++){
-			switch (part_gibs[i].name){
-				case "particles_blood":
-				part_gibs[i].Play();
-				break;
+		ParticleSystem myPart = scr_GameManager.gameManager.GetComponent<scr_GameManager>().GetSystem(gameObject, "particles_stars");
+		myPart.Play();
+		
+		myPart = scr_GameManager.gameManager.GetComponent<scr_GameManager>().GetSystem(gameObject, "particles_blood");
+		myPart.Play();
 
-				case "particles_death":
-				part_gibs[i].Play();
-				break;
-
-				case "particles_rainbow":
-				part_gibs[i].Stop();
-				break;
-			}
-		}
-
-		//part_blood.particleSystem.Play();
-			//= transform.Find ("particles_death").GetComponent<ParticleSystem> ().particleEmitter.emit = true;
+		myPart = scr_GameManager.gameManager.GetComponent<scr_GameManager>().GetSystem(gameObject, "particles_rainbow");
+		myPart.Stop();
+	
 		DestroyObject(mark);
 	}
 }
