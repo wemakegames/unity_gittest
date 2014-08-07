9686ef2ea3f58ba20c686e8bbbaf019ac565a259
andreadst <andreadst@hotmail.com>
2014-08-07 23:51:47 +0200

    converting gameplay to translations instead of phys

---
+++ Assets/scripts/PlayerPhysics.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPhysics : MonoBehaviour {
+
+	public void Move (Vector2 moveAmount){
+				
+		transform.Translate(moveAmount);
+	}
+
+}
